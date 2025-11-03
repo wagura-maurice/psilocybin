dd6befa6dfb5009dd897ae489686be2f7a125492
wagura Maurice <buisness@waguramaurice.com>
2025-11-03 22:00:45 +0300

    update app.js and bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,15 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 
+// Make Alpine globally available
 window.Alpine = Alpine;
 
+// Start Alpine when the page loads
+window.addEventListener('alpine:init', () => {
+    console.log('Alpine.js is initializing...');
+});
+
+// Start Alpine
 Alpine.start();
+console.log('Alpine.js initialized');
+
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,20 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
+
 window.axios = axios;
+window.Swal = Swal;
+
+// Add a simple toast function using SweetAlert2
+window.Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
