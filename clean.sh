4b8a247de35f6eba9b797bcf8ea3aaa5f4d79356
wagura Maurice <buisness@waguramaurice.com>
2025-11-03 22:01:44 +0300

    create 1 file and update 4 files

---
+++ clean.sh
@@ -0,0 +1,14 @@
+/usr/bin/php8.4 artisan down
+/usr/bin/php8.4 artisan cache:clear
+/usr/bin/php8.4 artisan route:clear
+/usr/bin/php8.4 artisan route:cache
+/usr/bin/php8.4 artisan config:clear
+/usr/bin/php8.4 artisan config:cache
+/usr/bin/php8.4 artisan view:clear
+/usr/bin/php8.4 artisan optimize
+/usr/bin/php8.4 artisan storage:unlink
+/usr/bin/php8.4 artisan storage:link
+/usr/bin/php8.4 artisan up
+/usr/bin/php8.4 /usr/local/bin/composer dump-autoload
+/usr/bin/php8.4 artisan migrate:fresh --seed
+npm run build
